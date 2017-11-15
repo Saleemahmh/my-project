797c56d72a5d61bda7ce2a94242ed7dd8f9f06eb
Saleemahmh <ssaleemah24@gmail.com>
2017-11-15 15:46:45 +0530

    Updation

--- src/App.vue
+++ src/App.vue
@@ -16,35 +16,3 @@ export default {
 }
 </script>
 
-<style lang="css">
-.panel {
-    margin: 5%;
-    background: transparent;
-  }
-  tr {
-    transition: all 0.5s;
-  }
-
-  tr:hover {
-    background-color: #f0ad4e;
-    transition: 0.5s;
-  }
-  body {
-
-    background-color: none;
-    font: oblique;
-    size: 18;
-    font-family: 'Didot-Italic';
-    font-style: italic;
-    color: #B22222;
-    text-align: center;
-  }
-
-  .panel-default {
-    background: transparent !important;
-    border: transparent !important;
-    /* font-family: 'Didot-Italic'; */
-    font-style: italic;
-    font-size: 20px;
-  }
-</style>
